dffc257502f8dfbb9508b03810d3f55b1fad231f
Ryan Smith <R-Smith@users.noreply.github.com>
2023-07-06 11:01:00 -0700

    Add custom message window

---
+++ Sysmon-Watcher/UI/MessageWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Sysmon_Watcher.UI
+{
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow(string message)
+        {
+            InitializeComponent();
+
+            Message.Text = message;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
